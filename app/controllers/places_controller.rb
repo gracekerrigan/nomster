f53bd764a82589a22d6f6622f144f1443de1c859
Rachael Grace Kerrigan <pestjungfrau1348@hotmail.com>
2019-06-20 04:15:55 +0000

    June19pt7

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,6 +27,14 @@ before_action :authenticate_user!, only: [:new, :create]
         @place.update_attributes(place_params)
         redirect_to root_path 
     end	
+	
+	def destroy
+	    @place = Place.find(params[:id])
+	    @place.destroy
+	    rediract_to root_path
+	end
+
+		
 	private
 
 	def place_params
