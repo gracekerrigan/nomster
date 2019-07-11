4d68fd371b6b497e5bb78f8bff2276f064226e8c
Rachael Grace Kerrigan <pestjungfrau1348@hotmail.com>
2019-07-11 02:03:31 +0000

    july10pt5

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,4 @@
 class Photo < ApplicationRecord
-  def create
-   create_tables :photos do |t|	
-	belongs_to :caption
-	belongs_to :place_id
-  end
-
-    add_index :photos, [:caption, :place_id]
-    add_index :photos, :place_id
-  end   
+	belongs_to :place
+	belongs_to :user    
 end
