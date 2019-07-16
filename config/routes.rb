ab056dbc064d749fbce07f593dfd6524081e8610
Rachael Grace Kerrigan <pestjungfrau1348@hotmail.com>
2019-07-16 22:28:18 +0000

    july16pt1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 	root 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users, only: :show
+
     resources :places do
     	resources :comments, only: :create
         resources :photos, only: :create	
