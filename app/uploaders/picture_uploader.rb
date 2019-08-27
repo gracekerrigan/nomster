1fc5e53f9c0c5eb7cd8ce21b25a81b7371670608
Rachael Grace Kerrigan <pestjungfrau1348@hotmail.com>
2019-08-27 22:37:22 +0000

    aug27pt15

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -29,7 +29,7 @@ class PictureUploader < CarrierWave::Uploader::Base
    #end
 
    Create different versions of your uploaded files:
-  version :thumb do
+     version :thumb do
      process resize_to_fit: [50, 50]
    end
 
